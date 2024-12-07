bae40b8aa0c784b54b5e922c3264305fa4600dd0
osakana0630 <osakana0630@gmail.com>
2024-12-07 14:51:46 +0900

    add go code

---
+++ go/excellent/main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestEvenOrOdd(t *testing.T) {
+  result := EvenOrOdd(10)
+  if result != "Even" {
+    t.Errorf("EvenOrOdd(10) = %s; want Even", result)
+  }
+}
