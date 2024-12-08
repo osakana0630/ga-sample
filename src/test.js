d297fb1ba9ae095dfd51c66db40a8d9ddbd02e95
Naoya Imai <59355224+osakana0630@users.noreply.github.com>
2024-12-08 14:22:51 +0900

    ffix (#10)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- src/test.js
+++ src/test.js
@@ -3,4 +3,9 @@ let param = "id";
 let value = "1001";
 let url = "https://" + domain + "?"+ param + "=" + value;
 
-console.log(url)
+console.log(url);
+console.log(url);
+console.log(url);
+console.log(url);
+console.log(url);
+console.log(url);
